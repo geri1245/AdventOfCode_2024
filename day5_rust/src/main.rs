711284fb187c3b3d3de583f8675a1d5281d6af02
Gergely Tarsoly <gergely.tarsoly3@gmail.com>
2024-12-05 19:23:01 +0100

    Add day 5 part 2

--- day5_rust/src/main.rs
+++ day5_rust/src/main.rs
@@ -19,6 +19,35 @@ fn do_numbers_match_rules(rules: &HashMap<u32, Vec<u32>>, numbers: &Vec<u32>) ->
         })
 }
 
+fn get_vec_without_number(vec: &Vec<u32>, number_to_exclude: u32) -> Vec<u32> {
+    vec.clone()
+        .into_iter()
+        .filter(|&elem| elem != number_to_exclude)
+        .collect::<Vec<u32>>()
+}
+
+fn num_of_numbers_contained_in_other(numbers: &Vec<u32>, other: &Vec<u32>) -> usize {
+    numbers
+        .iter()
+        .filter(|number| other.contains(number))
+        .count()
+}
+
+fn order_numbers_according_to_rules(rules: &HashMap<u32, Vec<u32>>, numbers: &Vec<u32>) -> u32 {
+    let num_count = numbers.len();
+    let mut ordered_numbers = vec![0; num_count];
+    for number in numbers {
+        if let Some(following_numbers) = rules.get(&number) {
+            let remaining_numbers = get_vec_without_number(numbers, *number);
+            let nums_contained =
+                num_of_numbers_contained_in_other(&remaining_numbers, following_numbers);
+            let result_index = num_count - 1 - nums_contained;
+            ordered_numbers[result_index] = *number;
+        }
+    }
+    ordered_numbers[num_count / 2]
+}
+
 fn main() -> io::Result<()> {
     let path = Path::new("input.txt");
 
@@ -59,8 +88,9 @@ fn main() -> io::Result<()> {
 
     let mut middle_number_sum = 0;
     for number_array in number_arrays {
-        if do_numbers_match_rules(&numbers_after_this_number, &number_array) {
-            middle_number_sum += number_array[number_array.len() / 2];
+        if !do_numbers_match_rules(&numbers_after_this_number, &number_array) {
+            middle_number_sum +=
+                order_numbers_according_to_rules(&numbers_after_this_number, &number_array);
         }
     }
 
